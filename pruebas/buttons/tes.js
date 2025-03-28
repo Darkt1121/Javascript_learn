7d36adb29b2c9be084095390d4702d89172eb27a
kevin rigs dionicio sanchez <97697147+Darkt1121@users.noreply.github.com>
2025-03-27 21:53:49 -0300

    ddd

--- pruebas/buttons/tes.js
+++ pruebas/buttons/tes.js
@@ -1,19 +1,4 @@
 
-// document.addEventListener ("prueba", function (){
-//     let btn_1 = document.querySelector('.js-btn-button_1')
-//     let btn_2 = document.querySelector('.js-btn-button_2')
-//     let p_chose = document.querySelector('.js_chose_p')
-
-//     btn_1.addEventListener ("click", function (){
-//         p_chose.textContent = 'You chose: heads'
-//     })
-
-//     btn_2.addEventListener("click", function (){
-//         p_chose.textContent = 'You chose : tails'
-//     })
-
-
-// } )
 
 
 
@@ -22,11 +7,38 @@ let btn_2 = document.querySelector('.js-btn-button_2');
 let p_chose = document.querySelector('.js_chose_p');
 
 
-function buttons() {
-    p_chose.textContent = 'You chose: heads';
+// function buttons() {
+//     p_chose.textContent = 'You chose: heads';
+// }
+
+// btn_1.addEventListener("click", buttons);
+
+
+// btn_2.addEventListener("click",buttons);
+
+
+
+
+function buttons(event) {
+    if (event.target === btn_1) {
+        p_chose.textContent = 'You chose: heads';
+    } else if (event.target === btn_2) {
+        p_chose.textContent = 'You chose: tails';
+    }
 }
 
 btn_1.addEventListener("click", buttons);
+btn_2.addEventListener("click", buttons);
 
 
-btn_2.addEventListener("click",buttons);
+
+
+
+
+let input = document.querySelector('.js_input');
+let btn3 = document.querySelector('.js_btn_submit');
+
+
+input.addEventListener("click", function(){
+    
+})
