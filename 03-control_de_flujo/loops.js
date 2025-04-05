0d158fc911ee750115a2c9250b1f2e0c2b014389
kevin rigs dionicio sanchez <97697147+Darkt1121@users.noreply.github.com>
2025-04-05 15:29:48 -0300

    gg

--- 03-control_de_flujo/loops.js
+++ 03-control_de_flujo/loops.js
@@ -1,7 +1,20 @@
 
 
-let tes = ["kevin","rigs",21,2003]
+// let tes = ["kevin","rigs",21,2003]
 
-console.log(tes.unshift(33))
-console.log(tes.shift())
-console.log(tes.length)
+// console.log(tes.unshift(33))
+// console.log(tes.shift())
+// console.log(tes.length)
+
+
+
+let tes = [1,2,3,4,5]
+let result = 0
+
+for(let i = 0; i < tes.length; i++){
+    let sums = tes[i]
+
+    result += sums 
+}
+
+console.log(result)
