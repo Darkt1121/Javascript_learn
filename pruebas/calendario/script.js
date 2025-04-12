9beab223cbfd265f44fed04bb15daf59103c843f
kevin rigs dionicio sanchez <97697147+Darkt1121@users.noreply.github.com>
2025-04-12 19:06:15 -0400

    Update script.js

--- pruebas/calendario/script.js
+++ pruebas/calendario/script.js
@@ -2,45 +2,45 @@
 /// `${}`
                //Ejercicio 1
 //---------------------------------------------------------------------------------
-let array = [{
-    name: 'kevin',
-    dueDate: '2003-09-12'
-},{
-    name : 'sanchez',
-    dueDate: '1993-03-06'
-}
+// let array = [{
+//     name: 'kevin',
+//     dueDate: '2003-09-12'
+// },{
+//     name : 'sanchez',
+//     dueDate: '1993-03-06'
+// }
 
-]
-renderTodoList()
-function renderTodoList(){    
-     let arrayResult = ''
-     for(let i = 0; i < array.length; i++){
-          const resultObjet = array[i]
-          const html = `<p>${resultObjet} 
-          <button onclick="array.splice(${i,1})
-           renderTodoList()">delete
-          </button>
-           </p>`
+// ]
+// renderTodoList()
+// function renderTodoList(){    
+//      let arrayResult = ''
+//      for(let i = 0; i < array.length; i++){
+//           const resultObjet = array[i]
+//           const html = `<p>${resultObjet} 
+//           <button onclick="array.splice(${i,1})
+//            renderTodoList()">delete
+//           </button>
+//            </p>`
 
-          arrayResult += html
-     }
-     console.log(arrayResult)
-     document.querySelector('.js_text_div').innerHTML = arrayResult
-}
+//           arrayResult += html
+//      }
+//      console.log(arrayResult)
+//      document.querySelector('.js_text_div').innerHTML = arrayResult
+// }
 
-let btn_1 = document.querySelector(".js_btn_add_1")
-let input_text = document.querySelector(".js_input_text")
+// let btn_1 = document.querySelector(".js_btn_add_1")
+// let input_text = document.querySelector(".js_input_text")
 
-btn_1.addEventListener("click", function(){
+// btn_1.addEventListener("click", function(){
 
-let name = input_text.value
-array.push(name)
-console.log(array)
+// let name = input_text.value
+// array.push(name)
+// console.log(array)
 
-input_text.value = ''
+// input_text.value = ''
 
-renderTodoList()
-})
+// renderTodoList()
+// })
 
                // FIN
 //----------------------------------------------------------------
@@ -76,6 +76,7 @@ let tes = nums.pop()
 let tes2 = nums.push(99)
 console.log(tes)
 console.log(nums)
+
                // FIN
 //----------------------------------------------
 
